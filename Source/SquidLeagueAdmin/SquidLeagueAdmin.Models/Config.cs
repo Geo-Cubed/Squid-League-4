8b5c4460de9897054dc3fff29bd6d819ab9f27c1
George <44912087+Geo-Cubed@users.noreply.github.com>
2020-12-31 21:15:52 +0000

    created config model

--- Source/SquidLeagueAdmin/SquidLeagueAdmin.Models/Config.cs
+++ Source/SquidLeagueAdmin/SquidLeagueAdmin.Models/Config.cs
@@ -4,5 +4,14 @@ namespace SquidLeagueAdmin.Models
 {
     public class Config
     {
+        public string DatabaseName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Port { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
     }
 }
