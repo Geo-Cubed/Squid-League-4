7e847d9c00bf2d1ed1aefa5f48be987a9f2bfb65
George <44912087+Geo-Cubed@users.noreply.github.com>
2021-01-05 17:36:20 +0000

    Started work on teams page
    Bound combo box to list of teams

--- Source/SquidLeagueAdmin/SquidLeagueAdmin.Models/Team.cs
+++ Source/SquidLeagueAdmin/SquidLeagueAdmin.Models/Team.cs
@@ -6,10 +6,15 @@ namespace SquidLeagueAdmin.Models
 {
     public class Team
     {
-        public int Id { get; set; }
+        public int Id;
 
-        public string TeamName { get; set; }
+        public string TeamName;
 
-        public int IsActive { get; set; }
+        public int IsActive;
+
+        public override string ToString()
+        {
+            return $"{TeamName} - {((IsActive == 1) ? "" : "not ")} active";
+        }
     }
 }
--- Source/SquidLeagueAdmin/SquidLeagueAdmin.UI/ViewModels/Teams/TeamViewModel.cs
+++ Source/SquidLeagueAdmin/SquidLeagueAdmin.UI/ViewModels/Teams/TeamViewModel.cs
@@ -4,6 +4,7 @@ using SquidLeagueAdmin.RepoFactory;
 using SquidLeagueAdmin.RepositoryInterface;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace SquidLeagueAdmin.UI.ViewModels.Teams
@@ -13,15 +14,44 @@ namespace SquidLeagueAdmin.UI.ViewModels.Teams
         public Team Model { get; set; }
 
         private IRepository<Team> repo;
-
-        public IEnumerable<Team> AllTeams;
+        private ObservableCollection<Team> AllTeams;
+        private string LabelText;
 
         public TeamViewModel()
         {
-            // TODO: Delegate command.
-
+            this.AllTeams = new ObservableCollection<Team>() { new Team() { Id = -1, TeamName = "New Team", IsActive = 0 } };
             this.repo = RepositoryFactory.GetTeamRepository("SQL");
-            this.AllTeams = repo.GetItems();
+            var items = repo.GetItems();
+            foreach (var team in items)
+            {
+                this.AllTeams.Add(team);
+            }
+            
+            this.Model = new Team() { TeamName = "Ducklings", IsActive = 1 };
+        }
+
+        public ObservableCollection<Team> Teams
+        {
+            get => this.AllTeams;
+            set => SetProperty(ref this.AllTeams, value);
+        }
+
+        public string TeamName
+        {
+            get => this.Model.TeamName;
+            set => SetProperty(ref this.Model.TeamName, value);
+        }
+
+        public bool IsActive
+        {
+            get => (this.Model.IsActive == 1) ? true : false;
+            set => SetProperty(ref this.Model.IsActive, (value == true) ? 1 : 0);
+        }
+
+        public string Label
+        {
+            get => this.LabelText;
+            set => SetProperty(ref this.LabelText, value);
         }
     }
 }
