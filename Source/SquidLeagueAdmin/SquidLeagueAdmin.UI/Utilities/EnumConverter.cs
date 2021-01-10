32b0fc765256240a9db111db9d0b6e3d1509ccbf
George <44912087+Geo-Cubed@users.noreply.github.com>
2021-01-10 21:03:15 +0000

    Tried adding converter to rank boxes

--- Source/SquidLeagueAdmin/SquidLeagueAdmin.UI/Utilities/EnumConverter.cs
+++ Source/SquidLeagueAdmin/SquidLeagueAdmin.UI/Utilities/EnumConverter.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Globalization;
-using System.Text;
 using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace SquidLeagueAdmin.Utilities
 {
-    public class EnumConverter : MarkupExtention, IValueConverter
+    public class EnumConverter : MarkupExtension, IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((Enum)value).GetAttributeOfType<DisplayAttribute>().Name;
+            try
+            {
+                var item = (Enum)value;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+
+            return ((Enum)value).GetDescription();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
