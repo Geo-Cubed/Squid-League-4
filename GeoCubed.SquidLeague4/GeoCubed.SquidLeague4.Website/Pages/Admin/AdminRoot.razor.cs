32560ed1550c0fcfcf9c2846cf847fe29e299ac7
George <44912087+Geo-Cubed@users.noreply.github.com>
2021-06-05 22:16:31 +0100

    work on seperate admin root page

---
+++ GeoCubed.SquidLeague4/GeoCubed.SquidLeague4.Website/Pages/Admin/AdminRoot.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GeoCubed.SquidLeague4.Website.Pages.Admin
+{
+    public partial class AdminRoot
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        protected void NavigateTo(string url)
+        {
+            this.NavigationManager.NavigateTo(url);
+        }
+    }
+}
--- GeoCubed.SquidLeague4/GeoCubed.SquidLeague4.Website/ViewModels/Admin/AdminMatchViewModel.cs
+++ GeoCubed.SquidLeague4/GeoCubed.SquidLeague4.Website/ViewModels/Admin/AdminMatchViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeoCubed.SquidLeague4.Website.ViewModels.Admin
 {
