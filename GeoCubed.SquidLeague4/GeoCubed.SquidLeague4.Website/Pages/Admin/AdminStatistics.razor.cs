962b05e65065e467ff3e36abfe473e191c226b08
George Paddon <sggpaddo@liverpool.ac.uk>
2021-08-01 09:32:26 +0100

    work on admin stats page

--- GeoCubed.SquidLeague4/GeoCubed.SquidLeague4.Website/Pages/Admin/AdminStatistics.razor.cs
+++ GeoCubed.SquidLeague4/GeoCubed.SquidLeague4.Website/Pages/Admin/AdminStatistics.razor.cs
@@ -1,5 +1,7 @@
 ﻿using GeoCubed.SquidLeague4.Website.Interfaces;
+using GeoCubed.SquidLeague4.Website.ViewModels.Admin;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 
 namespace GeoCubed.SquidLeague4.Website.Pages.Admin
 {
@@ -7,5 +9,10 @@ namespace GeoCubed.SquidLeague4.Website.Pages.Admin
     {
         [Inject]
         private IStatsDataService statsDataService { get; set; }
+
+        protected List<AdminStatsViewModel> allStats { get; set; }
+            = new List<AdminStatsViewModel>();
+
+        protected int selectedStatsId { get; set; }
     }
 }
