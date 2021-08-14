65bbb8a0d570294ea3fbcdb606a231fa3b683d7f
George <44912087+Geo-Cubed@users.noreply.github.com>
2021-08-14 09:26:02 +0100

    Create RankDistributionByMode.sql

---
+++ SQL/RankDistributionByMode.sql
@@ -0,0 +1,33 @@
+use `squid_league_4`;
+
+select 
+	RankTbl.`Rank` as 'Rank',
+    count(*) as 'Occurances'
+from
+	(
+		select
+			`sz_rank` as 'Rank',
+            '3' as 'game_mode'
+		from `player`
+		union all
+		select
+			`rm_rank` as 'Rank',
+            '2' as 'game_mode'
+		from `player`
+        union all
+		select
+			`cb_rank` as 'Rank',
+            '1' as 'game_mode'
+		from `player`
+        union all
+		select
+			`tc_rank` as 'Rank',
+            '4' as 'game_mode'
+		from `player`
+    ) as RankTbl
+where RankTbl.`Rank` <> 'NA'
+and `game_mode` <> 0
+group by RankTbl.`Rank`
+order by count(*) desc;
+
+    
